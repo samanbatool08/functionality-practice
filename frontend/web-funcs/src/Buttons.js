7b86d3474944d754bc2127617b807d8964575970
Your Name <samanbatool08@gmail.com>
2020-11-18 11:49:36 -0500

    case working

--- frontend/web-funcs/src/Buttons.js
+++ frontend/web-funcs/src/Buttons.js
@@ -4,17 +4,31 @@ class Buttons extends React.Component {
     
     state = {   
         increase: false, 
-        decrease: false
+        decrease: false,
+        number: 0
     }
 
+    increment = (e) => {
+        console.log('hitting')
+        console.log(e.target.value)
+
+        if(e.target.value === 'left'){
+            this.setState({
+                number: this.state.number - 1
+            })
+        
+        }
+        
+        
+    }
 
     render(){
         console.log('working')
         return (
             <div className='buttons'>
                 <h2>buttons</h2>
-                <button>6</button>
-                <button className='buttons__left'>Left</button>
+                <button className='buttons__number'>{this.state.number}</button>
+                <button className='buttons__left' onClick={this.increment} value='left'>Left</button>
                 <button className='buttons__right'>Right</button>
             </div>
         )
