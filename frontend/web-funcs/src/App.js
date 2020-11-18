50e28596a04df02aa3c643e8453e1975ee4b7102
Your Name <samanbatool08@gmail.com>
2020-11-18 13:23:32 -0500

    mapstate; mapdispatch to props

--- frontend/web-funcs/src/App.js
+++ frontend/web-funcs/src/App.js
@@ -12,12 +12,19 @@ function App() {
   );
 }
 
-const mysteryFunc1 = (mysArg1) => {
-  return {}
+const mapStateToProps = (state) => {
+  console.log('inside mysteryfunc1', state)
+  return {
+    // props based on state that app will receive
+  } 
 }
 
-const mysteryFunc2 = (mysArg2) => {
-  return {}
+const mapDispatchToProps = (dispatch) => {
+  console.log('inside mysteryfunc2', dispatch)
+  return {
+    // series of functions that app will be able to call that affect our store
+  }
 }
 
-export default connect(mysteryFunc1, mysteryFunc2) (App);
+export default connect(mapStateToProps, mapDispatchToProps ) (App);
+  
