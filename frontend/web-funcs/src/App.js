d88df16062f33d776845e8d2c62414bcba43ad01
Your Name <samanbatool08@gmail.com>
2020-11-18 13:57:36 -0500

    remove logic from app; store directly connecting to button component

--- frontend/web-funcs/src/App.js
+++ frontend/web-funcs/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import Buttons from './Buttons';
-import { connect } from 'react-redux';
 
 function App() {
   return (
@@ -12,19 +11,7 @@ function App() {
   );
 }
 
-const mapStateToProps = (state) => {
-  console.log('inside mysteryfunc1', state)
-  return {
-    // props based on state that app will receive
-  } 
-}
 
-const mapDispatchToProps = (dispatch) => {
-  console.log('inside mysteryfunc2', dispatch)
-  return {
-    // series of functions that app will be able to call that affect our store
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps ) (App);
+export default App;
   
