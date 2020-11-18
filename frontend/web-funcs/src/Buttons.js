ac79cf01f0e2dc09d2ba8f0e84fac6b704db7135
Your Name <samanbatool08@gmail.com>
2020-11-18 13:53:38 -0500

    dispatch working

--- frontend/web-funcs/src/Buttons.js
+++ frontend/web-funcs/src/Buttons.js
@@ -13,7 +13,7 @@ class Buttons extends React.Component {
                 <h2>buttons</h2>
                 <button className='buttons__number'>{this.props.number}</button>
                 <button className='buttons__left' onClick={this.increment} value='left'>Left</button>
-                <button className='buttons__right' onClick={this.increment} value='right'>Right</button>
+                <button className='buttons__right' onClick={this.props.increment} value='right'>Right</button>
             </div>
         )
     }
@@ -29,10 +29,30 @@ const msp = (state) => {
   const mdp = (dispatch) => {
     return {
       // series of functions that app will be able to call that affect our store
+        increment: () => dispatch({type: 'INCREMENT'})
     }
   }
 
 export default connect(msp, mdp) (Buttons);
 
-//  
+// state = {   
+//     number: 0
+// }
+
+// increment = (e) => {
+//     console.log('hitting')
+//     console.log(e.target.value)
+
+//     if(e.target.value === 'left'){
+//         this.setState({
+//             number: this.state.number - 1
+//         }) }
+//         else if(e.target.value === 'right'){
+//             this.setState({
+//                 number: this.state.number + 1
+//             })
+//         }
+    
+//     }
+    
     
