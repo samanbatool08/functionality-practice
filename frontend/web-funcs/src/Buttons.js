511a1a641c6a5c21c0837e007165d596227152e3
Your Name <samanbatool08@gmail.com>
2020-11-18 11:36:59 -0500

    class component for low level state

--- frontend/web-funcs/src/Buttons.js
+++ frontend/web-funcs/src/Buttons.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-function Buttons() {
-    return (
-        <div className='buttons'>
-            <h2>buttons</h2>
-            <button>6</button>
-            <button className='buttons__left'>Left</button>
-            <button className='buttons__right'>Right</button>
-        </div>
-    )
+class Buttons extends React.Component {
+    render(){
+
+        return (
+            <div className='buttons'>
+                <h2>buttons</h2>
+                <button>6</button>
+                <button className='buttons__left'>Left</button>
+                <button className='buttons__right'>Right</button>
+            </div>
+        )
+    }
 }
 
 export default Buttons
