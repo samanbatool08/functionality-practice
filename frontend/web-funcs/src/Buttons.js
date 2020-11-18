bf7ad23a60c3200166d6ef783bc63d2d9d8a92af
Your Name <samanbatool08@gmail.com>
2020-11-18 11:55:09 -0500

    second case

--- frontend/web-funcs/src/Buttons.js
+++ frontend/web-funcs/src/Buttons.js
@@ -15,12 +15,16 @@ class Buttons extends React.Component {
         if(e.target.value === 'left'){
             this.setState({
                 number: this.state.number - 1
-            })
+            }) }
+            else if(e.target.value === 'right'){
+                this.setState({
+                    number: this.state.number + 1
+                })
+            }
         
         }
         
-        
-    }
+    
 
     render(){
         console.log('working')
@@ -29,7 +33,7 @@ class Buttons extends React.Component {
                 <h2>buttons</h2>
                 <button className='buttons__number'>{this.state.number}</button>
                 <button className='buttons__left' onClick={this.increment} value='left'>Left</button>
-                <button className='buttons__right'>Right</button>
+                <button className='buttons__right' onClick={this.increment} value='right'>Right</button>
             </div>
         )
     }
