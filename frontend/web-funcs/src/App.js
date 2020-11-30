96beb9a2aba1ce319d61c0c75ce525eea08eaea0
Your Name <samanbatool08@gmail.com>
2020-11-29 21:17:14 -0500

    form

--- frontend/web-funcs/src/App.js
+++ frontend/web-funcs/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import React from 'react';
 import Buttons from './Buttons';
+import Form from './Form';
 
 function App() {
   return (
     <div className="app">
       <Buttons />
 
+      <Form />
     
     </div>
   );
